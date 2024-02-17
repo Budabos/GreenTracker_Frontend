dfde091e41be4e60f20d48af18c41ef562c3c702
caroline opiyo <opiyocaroline20@gmail.com>
2024-02-18 00:27:24 +0300

    Implemented routing configuration for the application using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from '../src/pages/Login'
 import Signup from '../src/pages/Signup'
 import AboutUs from '../src/pages/AboutUs'
 import ContactUs from '../src/pages/ContactUs'
+import Faqs from "../src/pages/Faqs";
+import Feedback from "../src/pages/Feedback";
 
 
 
@@ -19,6 +21,11 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/contact-us" element={<ContactUs />} />
+      <Route path="/faqs" element={<Faqs />} />
+      <Route path="/feedback" element={<Feedback />} />
+
+
+      
 
     </Routes>
   );
