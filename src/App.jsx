68ad4b8d806996ffa062373643d9e672908036ac
Miriam Wahito Maina <miriammaina51@gmail.com>
2024-02-18 19:22:09 +0300

    Added a form for donation details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "../src/pages/Login";
 import Signup from "../src/pages/Signup";
 import About from "../src/pages/About";
 import Footer from "./components/Footer";
+import Donation from "./pages/Donation";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/about" element={<About />} />
+        <Route path="/donations" element={<Donation />} />
       </Routes>
       <Footer />
     </>
