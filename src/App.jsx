71f83b8d006ad3fed53ed33b3b0b69b311ebf3a7
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-02-18 11:52:21 +0300

    Product page basic layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Home from '../src/pages/Home'
-import Login from '../src/pages/Login'
-import Signup from '../src/pages/Signup'
-import About from '../src/pages/About'
-
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Signup from "../src/pages/Signup";
+import About from "../src/pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -12,8 +12,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
 
-export default App
+export default App;
