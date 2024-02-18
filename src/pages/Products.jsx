7b42c0652addb29d9458b02fa648559636ba2e94
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-02-18 13:45:33 +0300

    Product pagination setup

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,6 +4,15 @@ import axios from "axios";
 import { Loader2 } from "lucide-react";
 import React, { useState } from "react";
 import { toast } from "sonner";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const Products = () => {
   const products = [
@@ -131,6 +140,16 @@ const Products = () => {
 
   const isLoading = false;
 
+  const [pageOffset, setPageOffset] = useState(0);
+  const endOffset = pageOffset + 9;
+  const renderedProducts = products.slice(pageOffset, endOffset);
+  const pageCount = Math.ceil(products.length / 9);
+
+  const handlePageClick = (pageNum) => {
+    const newOffset = (pageNum * 9) % products.length;
+    setPageOffset(newOffset);
+  };
+
   return (
     <div className="py-10 px-6">
       <h1 className="text-3xl font-bold">Available Products</h1>
@@ -140,10 +159,43 @@ const Products = () => {
           Loading products...
         </div>
       ) : (
-        <div className="grid grid-cols-3 gap-8 pt-8">
-          {products.map((product, index) => (
-            <ProductItem key={index} product={product} />
-          ))}
+        <div>
+          <div className="grid grid-cols-3 gap-8 pt-8">
+            {renderedProducts.map((product, index) => (
+              <ProductItem key={index} product={product} />
+            ))}
+          </div>
+          <Pagination className="mt-10">
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  className={pageOffset === 0 && "cursor-not-allowed"}
+                  onClick={() => setPageOffset(pageOffset - 9)}
+                />
+              </PaginationItem>
+              {new Array(pageCount).fill(0).map((_, index) => (
+                <PaginationItem>
+                  <PaginationLink
+                    isActive={index * 9 === pageOffset}
+                    onClick={() => handlePageClick(index)}
+                  >
+                    {index + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  className={
+                    pageOffset === (pageCount - 1) * 9 && "cursor-not-allowed"
+                  }
+                  onClick={() => setPageOffset(pageOffset + 9)}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       )}
     </div>
