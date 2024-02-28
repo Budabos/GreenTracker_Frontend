f2fd5808f8c3037f68106dbcacfc0b6ee543bbed
Ashley Rarieya <ararieya@gmail.com>
2024-02-28 14:29:50 +0300

    deleted Carbon Footprint page

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -47,10 +47,6 @@ const NavLinks = () => {
       route: "/events",
     },
  
-    {
-      text: "Calculate footprint",
-      route: "/footprint",
-    },
   ];
 
   const { getUser } = useAuth();
