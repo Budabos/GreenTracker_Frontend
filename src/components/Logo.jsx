1686181ca4e0b0b90ab97cc543c2ccc24a68ffd6
caroline opiyo <opiyocaroline20@gmail.com>
2024-02-29 23:53:40 +0300

    adding comments to logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+// Component for displaying the logo
 const Logo = () => {
-  return <div>Logo</div>;
+  return <div>Logo</div>; // Placeholder for logo content
 };
 
 export default Logo;
+
