1d98a36a554588ea73b62a50808c3003774c8e86
caroline opiyo <opiyocaroline20@gmail.com>
2024-02-29 21:36:18 +0300

    Added logo and styling to footer component

--- src/components/FootprintDisplay.jsx
+++ src/components/FootprintDisplay.jsx
@@ -1,8 +1,13 @@
 
+import React from "react";
+
+// Functional component for displaying footprint
 const FootprintDisplay = () => {
   return (
+    // Displaying a simple div with text
     <div>FootprintDisplay</div>
-  )
+  );
 }
 
-export default FootprintDisplay
+// Exporting the FootprintDisplay component
+export default FootprintDisplay;
