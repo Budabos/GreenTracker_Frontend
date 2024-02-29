79e6225dc477f32f5c3140300b8b931ec1cafbe8
caroline opiyo <opiyocaroline20@gmail.com>
2024-02-29 23:02:48 +0300

    updating forgotpassword

--- src/components/ForgotPassForm.jsx
+++ src/components/ForgotPassForm.jsx
@@ -21,11 +21,14 @@ import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { useAuth } from "@/providers/AuthProvider";
 
+// Define Zod schema for form validation
 const forgotPasswordSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email(), // Email field should be a valid email
 });
 
+// Component for Forgot Password Form
 const ForgotPassForm = () => {
+  // Initialize useForm hook with Zod resolver and default values
   const form = useForm({
     resolver: zodResolver(forgotPasswordSchema),
     defaultValues: {
@@ -33,28 +36,32 @@ const ForgotPassForm = () => {
     },
   });
 
+  // useMutation hook for handling form submission
   const { mutate, isPending } = useMutation({
     mutationKey: ["forgot-password"],
     mutationFn: async (values) => {
       return await axios
-        .post(`${BASE_URL}/forgot-password`, values)
+        .post(`${BASE_URL}/forgot-password`, values) // Send POST request to forgot-password endpoint
         .then((res) => {
           toast.success(res.data.message);
-          form.reset();
+          form.reset(); // Reset form after successful submission
         })
         .catch((err) => {
-          toast.error(err.response.data.message);
+          toast.error(err.response.data.message); // Display error message if submission fails
         });
     },
   });
 
+  // Function to handle form submission
   function onSubmit(values) {
-    mutate(values);
+    mutate(values); // Call mutation function with form values
   }
 
   return (
     <Form {...form}>
+      {/* Form wrapper */}
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* Email field */}
         <FormField
           control={form.control}
           name="email"
@@ -73,6 +80,7 @@ const ForgotPassForm = () => {
         />
         <div className="flex flex-col items-start">
           <Link
+            {/* Links for existing and new users */}
             to={"/login"}
             className={cn(
               buttonVariants({
@@ -94,6 +102,7 @@ const ForgotPassForm = () => {
           >
             Don&apos;t have an account?
           </Link>
+          {/* Submit button */}
           <Button
             className="bg-black text-white"
             type="submit"
